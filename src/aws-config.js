a866d857b7b123f50b91583f05c6d40922ac6871
callow <73476496+openscallow@users.noreply.github.com>
2024-09-21 15:58:09 +0530

    trying to implement env var

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,9 +1,9 @@
-export const awsConfig = {
-    region: 'ap-south-1',
-    credentials: {
-      accessKeyId: 'AKIAZQ3DRTNPTI6P7YQU',
-      secretAccessKey: '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'
-    }
-  };
+import { env } from '$env/dynamic/private';
 
-  
+export const awsConfig = {
+  region: 'ap-south-1',
+  credentials: {
+    accessKeyId: env.BUCKET_ACCESS_KEY_ID,
+    secretAccessKey: env.BUCKET_SECRET_ACCESS_KEY
+  }
+};
