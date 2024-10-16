3aecc782293dea6a126a448574920e7dba130e63
Gautam <kaal2045@gmail.com>
2024-10-16 18:43:23 +0530

    integrate dotenv / envirment variables locally

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,9 +1,18 @@
 import { env } from '$env/dynamic/private';
+import 'dotenv/config';
+
+const accessKeyId = process.env.BUCKET_ACCESS_KEY_ID 
+  ? process.env.BUCKET_ACCESS_KEY_ID 
+  : env.BUCKET_ACCESS_KEY_ID;
+
+const secretAccessKey = process.env.BUCKET_SECRET_ACCESS_KEY 
+  ? process.env.BUCKET_SECRET_ACCESS_KEY 
+  : env.BUCKET_SECRET_ACCESS_KEY;
 
 export const awsConfig = {
   region: 'ap-south-1',
   credentials: {
-    accessKeyId: env.BUCKET_ACCESS_KEY_ID,
-    secretAccessKey: env.BUCKET_SECRET_ACCESS_KEY
+    accessKeyId,
+    secretAccessKey
   }
 };
