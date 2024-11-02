c6ec760e894d9cda29289b704fc1161f83a6e943
Gautam <kaal2045@gmail.com>
2024-11-02 17:57:24 +0530

    just fucked up

--- src/lib/components/recentlyViewed/component.svelte
+++ src/lib/components/recentlyViewed/component.svelte
@@ -59,7 +59,7 @@
       <div class="swiper-wrapper">
         {#each $recentlyViewed as item}
           <div class="swiper-slide">
-            <div class="tile mt-2">
+            <div class="tile mt-2 min-h-[360px]">
               <a 
                 href="/{item.id}" 
                 rel="noopener noreferrer"
