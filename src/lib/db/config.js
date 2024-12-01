f427cece539dd2f7fe94a7b0f75d4193de10928c
Gautam <kaal2045@gmail.com>
2024-12-01 18:54:08 +0530

    vercel env

--- src/lib/db/config.js
+++ src/lib/db/config.js
@@ -1,18 +1,21 @@
+import { env } from '$env/dynamic/private';
+
 import dotenv from 'dotenv';
+dotenv.config();
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
-dotenv.config();
 
 const dbConfig = {
     user: process.env.USERX,
-    host: process.env.HOST,
+    host: process.env.HOST || env.HOST,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
-};
+}; 
 
 export const pool = new Pool(dbConfig);
