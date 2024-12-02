d812a682e3d1c38892ec29ef4c77362bdba9c6cb
Gautam <kaal2045@gmail.com>
2024-12-02 10:31:02 +0530

    fuck off error

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,13 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	prerender: {
+		handleHttpError: ({ path, referrer, error }) => {
+		  // Custom error handling
+		  if (path === '/login') return; // Ignore login route
+		}
+	  }
 };
 
 export default config;
