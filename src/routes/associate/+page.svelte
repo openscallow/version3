31c7399b42d9f8624df2cab0d02ae09450abd499
Gautam <kaal2045@gmail.com>
2024-12-08 15:11:23 +0530

    initial delevery boy

---
+++ src/routes/associate/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    import '@tailwind'
+    import { MoveRight } from 'lucide-svelte';
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+
+	const options = [
+		{ time: '08:00 am', schoolIndex:123, school: 'jay ambe vidhya bhavan' },
+		{ time: '09:00 am', schoolIndex:13, school: 'veer bhamasha' },
+
+	];
+</script>
+
+<Timeline>
+	{#each options as option}
+		<TimelineItem>
+			<TimelineOppositeContent slot="opposite-content">
+				<p>{option.time}</p>
+			</TimelineOppositeContent>
+			<TimelineSeparator>
+				<TimelineDot />
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+                
+			
+                <div class="collapse bg-base-200">
+                    <input type="radio" name="my-accordion-2"  />
+                    <div class="collapse-title text-xl font-medium">{option.school}</div>
+                    <div class="collapse-content">
+                        <a href="associate/{option.schoolIndex}" rel="noopener noreferrer">
+                            <p><MoveRight /></p>
+                        </a>
+                    </div>
+                  </div>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
+
+
