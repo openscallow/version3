d6741a8a9017fb4ef2018f61f15d7a9fb4bf76e5
Gautam <kaal2045@gmail.com>
2025-01-01 05:43:47 +0530

    insert: Development Classes schedule

--- src/routes/orderHistory/deliverySchedule.js
+++ src/routes/orderHistory/deliverySchedule.js
@@ -1,38 +1,38 @@
 const deliverySchedule = {
   Monday: { 
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" }
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   },
   
   Tuesday: { 
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" } 
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" } 
   },
 
   Wednesday: { 
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" } 
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   },
 
   Thusrday: {
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" }
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   },
 
   Friday: {
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" }
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   },
 
   Saturday: {
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" }
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   },
   Sunday: {
     "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Sunrise School": { start: "8:30 AM", end: "9:00 AM" }
+    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
   }
-};
+}; 
   
 
   
