8279d412c0a98b3c9a9840bff56ea3e4d619da60
Gautam <kaal2045@gmail.com>
2025-01-01 12:45:52 +0530

    Typo: fix thursday

--- src/routes/orderHistory/deliverySchedule.js
+++ src/routes/orderHistory/deliverySchedule.js
@@ -1,36 +1,36 @@
 const deliverySchedule = {
   Monday: { 
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   },
   
   Tuesday: { 
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" } 
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" } 
   },
 
   Wednesday: { 
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   },
 
-  Thusrday: {
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+  Thursday: {
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   },
 
   Friday: {
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   },
 
   Saturday: {
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   },
   Sunday: {
-    "Matrubhumi Vidhya Bhavan": {start: "11:55 AM", end: "12:05 PM"},
-    "Development Classes": { start: "11:45 AM", end: "11:55 AM" }
+    "Matrubhumi Vidhya Bhavan": {start: "10:55 AM", end: "12:05 PM"},
+    "Development Classes": { start: "10:45 AM", end: "10:55 AM" }
   }
 }; 
   
