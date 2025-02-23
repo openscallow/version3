45b16a25e7b9c478fb12a0e74d6b4e5326c411db
Gautam <kaal2045@gmail.com>
2025-02-23 09:48:56 +0530

    minor change

--- src/lib/components/gridtiles/grid-tiels.svelte
+++ src/lib/components/gridtiles/grid-tiels.svelte
@@ -4,8 +4,7 @@
   import { getProductsWithCache } from './cacheUtils.js';
   import '$lib/components/gridtiles/style.css'
   import RecentDrops from '$lib/components/recentDrops.svelte';
-  import Trending from '$lib/components/trending.svelte';
-  import MostPurchased from '$lib/components/mostPurchased.svelte';
+    
 
   </script>
   <RecentDrops/>
