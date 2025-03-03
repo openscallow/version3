ccaa5319b49ed1a06f736dbeb8fa7ff0fa4d3fa8
Gautam <kaal2045@gmail.com>
2025-03-03 19:27:43 +0530

    some file delete and create

--- src/lib/components/gridtiles/grid-tiels.svelte
+++ src/lib/components/gridtiles/grid-tiels.svelte
@@ -1,11 +1,6 @@
 <script>
-
-  import { onMount } from 'svelte';
-  import { getProductsWithCache } from './cacheUtils.js';
   import '$lib/components/gridtiles/style.css'
   import RecentDrops from '$lib/components/recentDrops.svelte';
-    
-
   </script>
   <RecentDrops/>
 
