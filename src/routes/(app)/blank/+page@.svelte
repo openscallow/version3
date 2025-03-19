4b61ed0ea30c4a68cc891525910ae436198385ea
Gautam <kaal2045@gmail.com>
2025-03-19 15:57:33 +0530

    blank/search page initial commit

--- src/routes/(app)/blank/+page@.svelte
+++ src/routes/(app)/blank/+page@.svelte
@@ -1,1 +1,23 @@
-hi
+<script lang="ts">
+    import './style.css';
+    import { Search } from "lucide-svelte";
+
+</script>
+<div class="wrapper">
+    <input type="text">
+    <div class="wrapper_search_icon">
+        <Search class="search_icon"/>
+    </div>   
+</div>
+
+<div class="category">
+    <h2>Popular</h2>
+    <div class="category_list">
+        <div class="category_item">Cello gripper pens pens</div>
+        <div class="category_item">Category 2</div>
+        <div class="category_item">Category 3</div>
+        <div class="category_item">Category 4</div>
+        <div class="category_item">Category 5</div>
+    </div>
+</div>
+
