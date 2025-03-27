d8666850e1906ae4d352e4c976d90ac9d0fa2abf
Gautam <kaal2045@gmail.com>
2025-03-27 16:28:32 +0530

    few std add

--- src/routes/(app)/search/+page@.svelte
+++ src/routes/(app)/search/+page@.svelte
@@ -19,8 +19,10 @@
 <div class="category">
     <h2>Popular</h2>
     <div class="category_list">
-        <div class="category_item"><a href="/search/result?k=Cello gripper pens">Cello gripper pens</a></div>
-        <div class="category_item"><a href="/search/result?k=STD 12 SCIENCE">STD 12</a></div>
+        <div class="category_item"><a class="outline-none" href="/search/result?k=Cello gripper pens">Cello gripper pens</a></div>
+        <div class="category_item"><a class="outline-none" href="/search/result?k=STD 12 SCIENCE">STD 12 SCIENCE</a></div>
+        <div class="category_item"><a class="outline-none" href="/search/result?k=STD 12 COMMERCE">STD 12 COMMERCE</a></div>
+        <div class="category_item"><a class="outline-none" href="/search/result?k=STD 12 ARTS">STD 12 ARTS</a></div>
         <div class="category_item">Category 2</div>
         <div class="category_item">Category 3</div>
         <div class="category_item">Category 4</div>
