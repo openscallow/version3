d39198d792ce4973576c4557ce4d82811e529da0
Gautam <kaal2045@gmail.com>
2025-03-27 15:42:55 +0530

    std 12 inserted

--- src/routes/(app)/search/+page@.svelte
+++ src/routes/(app)/search/+page@.svelte
@@ -20,6 +20,7 @@
     <h2>Popular</h2>
     <div class="category_list">
         <div class="category_item"><a href="/search/result?k=Cello gripper pens">Cello gripper pens</a></div>
+        <div class="category_item"><a href="/search/result?k=STD 12 SCIENCE">STD 12</a></div>
         <div class="category_item">Category 2</div>
         <div class="category_item">Category 3</div>
         <div class="category_item">Category 4</div>
