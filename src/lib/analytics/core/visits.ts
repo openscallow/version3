dcd46378ea583e1e9e7b256525291a36dffab9dd
Gautam <kaal2045@gmail.com>
2025-09-22 22:36:35 +0530

    referral policy  &  analytics demo file

--- src/lib/analytics/core/visits.ts
+++ src/lib/analytics/core/visits.ts
@@ -4,32 +4,40 @@
  * Created by: Gautam mer (CEO)
  * Created at: 26/08/2025
  * 
- * Last by: Gautam mer (CEO)
- * Created at: 26/08/2025
- * 
-*/
-
-/**
- * 
+ * Last edit by: Gautam mer (CEO)
+ * Edited at: 21/09/2025
  * 
 */
 
 import { customerId } from '$lib/components/ts/customer_correlated.svelte';
+import { api } from '$lib/utils/api';
 
 export async function visit() {
+
+    // check session first so it want update every time when user referesh page 
+    if(sessionStorage.getItem('sessionContinuity')) return;
+    sessionStorage.setItem('sessionContinuity', 'true')
+    
     let customer_id = customerId()
 
     // if visitor is not customer do nothing
     if(!customer_id) return;
 
+    // generate date format
+    const date = new Date(); // Current date
+    
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() is 0-based
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
+
     // else register necessary field in document
     try {
-        
+        const result = await api.post('/visit', {customer_id, formattedDate});
+        console.log(result)
     } catch (error) {
         
     }
-
-
-
 }
 
