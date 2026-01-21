80ca45361649c35892d66cab1d64653981c830a7
Gautam <kaal2045@gmail.com>
2026-01-21 20:54:51 +0530

    fix: promotion status based slection qury update with time zone so now reflect on kolkatta

--- src/routes/(app)/api/customers/[customerId]/promotions/+server.js
+++ src/routes/(app)/api/customers/[customerId]/promotions/+server.js
@@ -9,29 +9,30 @@ export async function GET({ url, params }){
 
     try {
         const query = `
-                    SELECT
-                        pc.id AS promotion_id,
-                        pc.code,
-                        pc.descriptions,
-                        pc.discount_type,
-                        pc.discount_value,
-                        pc.max_discount_cap,
-                        
-                        COALESCE(upt.status, 'eligible') AS status,
-                        upt.enrolled_at,
-                        upt.deadline_at,
-                        upt.progress_count
-                    FROM public.promotion_campaigns pc
-                    LEFT JOIN public.customer_promotion_tasks upt
-                        ON upt.promotion_id = pc.id
-                       AND upt.customer_id = $1
-                    WHERE
-                        upt.customer_id = $1 
-                        AND pc.is_active = TRUE
-                        AND NOW() BETWEEN pc.starts_at AND pc.expires_at
-                        AND COALESCE(upt.status, 'eligible') = $2;`;
+                    SELECT 
+	pc.id AS promotion_id,
+    pc.code,
+    pc.descriptions,
+    pc.discount_type,
+    pc.discount_value,
+    pc.max_discount_cap,
+	
+    cpt.enrolled_at,
+    cpt.deadline_at,
+    cpt.progress_count,
+	cpt.qualified_at
+FROM promotion_campaigns pc 
+JOIN customer_promotion_tasks cpt ON cpt.promotion_id = pc.id
+WHERE 
+	pc.is_active = TRUE
+	AND cpt.customer_id = $1
+	AND COALESCE(cpt.status, 'eligible') = $2
+	AND (NOW() AT TIME ZONE 'Asia/Kolkata') BETWEEN pc.starts_at AND pc.expires_at;`;
+
+                  
 
     let result = await pool.query(query, [customerId, status]);
+    console.log(result, 'server')
     
     return new Response(JSON.stringify(result.rows), {
         status: 200
