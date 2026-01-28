91cdc113b98f63329670fa440ea638940faa38b0
Gautam <kaal2045@gmail.com>
2026-01-28 19:22:59 +0530

    fix: promotions server file query improved

--- src/routes/(app)/api/customers/[customerId]/promotions/+server.js
+++ src/routes/(app)/api/customers/[customerId]/promotions/+server.js
@@ -8,28 +8,25 @@ export async function GET({ url, params }){
     if (!customerId) throw error(400, { message: 'Customer ID is required' });
 
     try {
-        const query = `
-                    SELECT 
-	pc.id AS promotion_id,
-    pc.code,
-    pc.descriptions,
-    pc.discount_type,
-    pc.discount_value,
-    pc.max_discount_cap,
-	
-    cpt.enrolled_at,
-    cpt.deadline_at,
-    cpt.progress_count,
-	cpt.qualified_at
-FROM promotion_campaigns pc 
-JOIN customer_promotion_tasks cpt ON cpt.promotion_id = pc.id
-WHERE 
-	pc.is_active = TRUE
-	AND cpt.customer_id = $1
-	AND COALESCE(cpt.status, 'eligible') = $2
-	AND (NOW() AT TIME ZONE 'Asia/Kolkata') BETWEEN pc.starts_at AND pc.expires_at;`;
+        const query = `SELECT 
+                        pc.id AS promotion_id,
+                        pc.code,
+                        pc.descriptions,
+                        pc.discount_type,
+                        pc.discount_value,
+                        pc.max_discount_cap,
 
-                  
+                        cpt.enrolled_at,
+                        cpt.deadline_at,
+                        cpt.progress_count,
+	                    cpt.qualified_at
+                    FROM promotion_campaigns pc 
+                    JOIN customer_promotion_tasks cpt ON cpt.promotion_id = pc.id
+                    WHERE 
+	                    pc.is_active = TRUE
+	                    AND cpt.customer_id = $1
+	                    AND COALESCE(cpt.status, 'eligible') = $2
+	                    AND (NOW() AT TIME ZONE 'Asia/Kolkata') BETWEEN pc.starts_at AND pc.expires_at;`;
 
     let result = await pool.query(query, [customerId, status]);
     console.log(result, 'server')
@@ -46,12 +43,4 @@ WHERE
             headers: { "Content-Type": "application/json" }
         });
     }
-
-    return new Response(JSON.stringify({ message: "Failed to fetch promotions" }), {
-            status: 500,
-            headers: { "Content-Type": "application/json" }
-        });
-    
-    
-    
 }
