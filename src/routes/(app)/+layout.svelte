1af79f8ea395d26b3695f1242dfe551f447ffe85
Gautam <kaal2045@gmail.com>
2026-02-06 08:58:19 +0530

    chore: json+ld structure inserted

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,16 +1,4 @@
 <script>
-/**
- * @File_Meta_data
- * 
- * Created by: Gautam mer (CEO)
- * Created at: initial file
- * 
- * Last edit by: Gautam mer (CEO)
- * Edited at: 11/12/2025
- * Last change: Replaced the old relative app.css import with the new alias-based @styles import.
- * 
-*/
-
 import '@styles';
 import Navbar from '$lib/components/navbar.svelte';
 import Footer from '$lib/components/svelte/footer.svelte';
@@ -34,6 +22,17 @@ $effect(() => {
 	}
 });
 </script>
+
+<svelte:head>
+	<script type="application/ld+json">
+	{
+		"@context": "https://schema.org",
+  		"@type": "Organization",
+  		"name": "Callow",
+  		"url": "https://callow.in"
+	}
+	</script>
+</svelte:head>
 <Navbar />
 
 <div style="padding-bottom: 5rem;">
